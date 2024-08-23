ee6d56ddda16330dbc38ab885ef1ce71c11d5f05
Joe simpson <joe@egmrc.com>
2024-08-23 15:48:48 -0600

    Add email

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from 'express'
 import payload from 'payload'
+let nodemailer = require("nodemailer");
+let aws = require("@aws-sdk/client-ses");
+let { defaultProvider } = require("@aws-sdk/credential-provider-node");
+
+const ses = new aws.SES({
+  apiVersion: "2010-12-01",
+  region: "us-east-2",
+  defaultProvider,
+})
+
+// create Nodemailer SES transporter
+let transporter = nodemailer.createTransport({
+  SES: { ses, aws },
+});
 
 require('dotenv').config()
 const app = express()
@@ -17,6 +31,15 @@ const start = async () => {
     onInit: async () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
     },
+    ...(process.env.AWS_SECRET_ACCESS_KEY
+      ? {
+        email: {
+          fromName: "Crazy Horse Memorial",
+          fromAddress: "noreply@crazyhorsememorial.org",
+          transport: transporter
+        },
+      }
+      : {}),
   })
 
   // Add your own express routes here
