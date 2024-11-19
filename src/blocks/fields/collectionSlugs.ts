a0a6b43bc8841ba3d8246b112cce3e840d28e552
Joe simpson <joe@egmrc.com>
2024-11-19 09:08:28 -0700

    Remove collections

--- src/blocks/fields/collectionSlugs.ts
+++ src/blocks/fields/collectionSlugs.ts
@@ -1,43 +1,8 @@
 export const collectionSlugs = [
     "pages",
-    "museumCollections",
-    "impact",
-    "passions",
-    "studentSpotlight",
-    "support",
-    "event",
-    "eventCat",
-    "news",
     "media"
 ]
 
 export const collectionSlugsOptions = [
     {label:"Pages", value:"pages"},
-    {
-        label: "Museum Collections",
-        value:"museumCollections"},
-    {
-        label: "Continuing To Impact",
-        value: "impact"
-    },
-    {
-        label: "Passions For The Project",
-        value: "passions",
-    },
-    {
-        label: "Student Spotlight",
-        value: "studentSpotlight"
-    },
-    {
-        label: "Our Support of Native Causes",
-        value: "support"
-    },
-    {
-        label: "Event",
-        value: "event"
-    },
-    {
-        label: "News",
-        value: "news"
-    }
 ]
